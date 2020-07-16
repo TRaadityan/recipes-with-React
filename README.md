805db13fcde977e0e9df4acfd6aeadaab0cc559b
TRaadityan <tv.aadit@gmail.com>
2020-07-16 15:13:04 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# recipes-with-React
---
+++ precache-manifest.f5472c4b97a3117d7f963351bf8f65cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5dbada9d1f9f720e47c735d3bd7797",
+    "url": "/recipes-with-React/index.html"
+  },
+  {
+    "revision": "8ab8d1c1f8ca6645caab",
+    "url": "/recipes-with-React/static/css/main.d036a4de.chunk.css"
+  },
+  {
+    "revision": "8a7ecce94932a6d69e05",
+    "url": "/recipes-with-React/static/js/2.5d1cc4bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-with-React/static/js/2.5d1cc4bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab8d1c1f8ca6645caab",
+    "url": "/recipes-with-React/static/js/main.a6c34583.chunk.js"
+  },
+  {
+    "revision": "9026b597a870a59ac40f",
+    "url": "/recipes-with-React/static/js/runtime-main.43b90452.js"
+  }
+]);
